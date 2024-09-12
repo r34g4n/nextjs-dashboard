d6907f53fd2648a8247fb9c69180fb80f4ab077e
Rigan Gor <rigan.gor@gmail.com>
2024-09-12 23:35:41 +0300

    Remove experimental typedRoutes from next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    experimental: {
-        typedRoutes: true,
-      },
+    // experimental: {
+    //     typedRoutes: true,
+    //   },
 };
 
 export default nextConfig;
